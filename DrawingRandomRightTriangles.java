ec77b68622e7224bb954c58e5e0f08b4d1b11d32
Bismark <bimarkeshun@gmail.com>
2016-03-29 16:02:16 +0100

    Assignment3_10532440

--- DrawingRandomRightTriangles.java
+++ DrawingRandomRightTriangles.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package assignment_10528808;
-
 import java.awt.Color;
 import java.awt.Canvas;
 import java.awt.Graphics;
@@ -12,10 +5,7 @@ import java.awt.Polygon;
 import java.util.Random;
 import javax.swing.JFrame;
 
-/**
- *
- * @author Mawutor
- */
+
 public class DrawingRandomRightTriangles 
 {
      Random r= new Random();
